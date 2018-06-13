02cdba2663dd33171233776fac9a2a6001102c2e
Kaitlin Kelly <kk504408@gmail.com>
2018-06-13 00:50:12 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,8 +6,8 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    @artist = Artist.find_or_create_by(name: name)
-    self.artist = @artist
+    a = Artist.find_or_create_by(name: name)
+    self.artist = a
   end
 
 
